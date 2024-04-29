779dcbec1639595c2194d8abcedfbdce5bf0f4c2
sagarbawap <sagarb91@outlook.com>
2024-04-28 23:15:19 -0500

    Update ResourceGroups.tf

--- ResourceGroups.tf
+++ ResourceGroups.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "RG-Sagar" {
   name     = "RG-Sagar"
   location = "East US2"
 }
+
+resource "azurerm_resource_group" "RG-new" {
+  name     = "RG-new"
+  location = "East US2"
+}
