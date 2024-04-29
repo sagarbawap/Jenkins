c866e99f7d25e9068dce7cc7c44222a5e5735511
sagarbawap <sagarb91@outlook.com>
2024-04-29 18:55:02 -0500

    update

--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -24,7 +24,7 @@ resource "azurerm_virtual_network" "vnet" {
   subnet {
     name           = "subnet2"
     address_prefix = "10.0.2.0/24"
-    security_group = azurerm_network_security_group.example.id
+    security_group = azurerm_network_security_group.NSG.id
   }
 
   tags = {
