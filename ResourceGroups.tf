f49e35529f2b7f26dc1d246d919d6202fbd60349
sagarbawap <sagarb91@outlook.com>
2024-05-01 22:43:07 -0500

    new rg created

--- ResourceGroups.tf
+++ ResourceGroups.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "RG-new" {
   name     = "RG-new"
   location = "East US2"
 }
+
+resource "azurerm_resource_group" "RG1" {
+  name     = "RG1"
+  location = "East US2"
+}
