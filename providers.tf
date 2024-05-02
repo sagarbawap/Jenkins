ee4320f17798f2303d14741a949b863f8e6cda4e
sagarbawap <sagarb91@outlook.com>
2024-05-01 22:29:18 -0500

    completed backend storage details

--- providers.tf
+++ providers.tf
@@ -3,6 +3,13 @@
 # Azure Provider source and version being used
 terraform {
 required_version = ">= 1.5.7"
+backend "azurerm" {
+  resource_group_name = "RG-Storge"
+  storage_account_name = "stgsagar6262"
+  container_name = "jenkinstfstate"
+  key = "jenkinstfstate.tfstate"
+  access_key = "JAtpZjLiIqm9jfsvhrY6agGeendKhnaF+zwUPQnEnRnn6jUxRghuIZpaz2n9U/k1mFlkScJeMF8C+AStzUEMpQ=="
+}
 }
 
 # Configure the Microsoft Azure Provider
