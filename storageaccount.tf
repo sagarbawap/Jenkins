d1000adc3eb0bddaa199e1c151122732d8e91154
sagarbawap <sagarb91@outlook.com>
2024-05-01 22:33:39 -0500

    Update storageaccount.tf

--- storageaccount.tf
+++ storageaccount.tf
@@ -1,8 +1,8 @@
-resource "azurerm_storage_account" "stgnew001" {
-  name                     = "stgnew001"
-  resource_group_name      = azurerm_resource_group.RG-Sagar.name
-  location                 = azurerm_resource_group.RG-Sagar.location
-  account_tier             = "Standard"
-  account_replication_type = "GRS"
+# resource "azurerm_storage_account" "stgnew001" {
+#   name                     = "stgnew001"
+#   resource_group_name      = azurerm_resource_group.RG-Sagar.name
+#   location                 = azurerm_resource_group.RG-Sagar.location
+#   account_tier             = "Standard"
+#   account_replication_type = "GRS"
 
-}
+# }
